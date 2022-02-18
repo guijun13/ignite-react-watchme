63e85cac00e180b88355f3cd7c0574c9373fc6da
guijun13 <guilhermejun13.grillo@gmail.com>
2022-02-17 23:27:25 -0300

    feat: refactor SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import React from 'react';
+import { Button } from './Button';
+
+interface GenreProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  };
+}
+
+interface OtherProps {
+  handleClickButton: (event: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: GenreProps[] & OtherProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
